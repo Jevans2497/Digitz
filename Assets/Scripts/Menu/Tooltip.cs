3054d72caa45f946565b316a25af3c791c585b03
Jevans2497 <Jevans2497@gmail.com>
2025-02-02 19:16:34 -0700

    upgrade display

--- Assets/Scripts/Menu/Tooltip.cs
+++ Assets/Scripts/Menu/Tooltip.cs
@@ -8,10 +8,12 @@ public class Tooltip: MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
     public string message;
 
     public void OnPointerEnter(PointerEventData eventData) {
+        Debug.Log($"Pointer Entered {gameObject.name}");
         TooltipManager.instance.setAndShowTooltip(message, this.transform.position);
     }
 
     public void OnPointerExit(PointerEventData eventData) {
+        Debug.Log($"Pointer Exited {gameObject.name}");
         TooltipManager.instance.hideTooltip();
     }
 }
